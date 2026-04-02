bef437e8d47ecfe152d693f7dcb79e62fe83aa26
antfullstack <253273528+antfullstack@users.noreply.github.com>
2026-04-02 13:37:08 +0300

    Added function for factorial

--- algebra.py
+++ algebra.py
@@ -1,4 +1,12 @@
 def power(base, exponent):
     return base ** exponent
 
+def factorial(number):
+    #Takes a number and until it is equal to 0 it multiplies it with its previous in order storing the new result
+    result = 1
+    while number > 0:
+        result *= number
+        number -= 1    
+    return result
 
+print(factorial(5))
