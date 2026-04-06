7defbcd5e72ccb2d13704cc553f08f454e5814a8
antfullstack <253273528+antfullstack@users.noreply.github.com>
2026-04-06 16:03:55 +0300

    Closes #75: Added interpolation and binary search

--- simple_equ/cs/locating.py
+++ simple_equ/cs/locating.py
@@ -23,3 +23,41 @@ def lowest_algorithm(arr: list[int] | tuple[int]) -> int:
         if num < lowest:
             lowest = num
     return lowest
+
+def binary_search(arr: list[int] | tuple[int], target: int):
+    """
+        Finds a target element's index. Array must be sorted for the search to work. O(log n) time complexity. 
+    """
+    low = 0 
+    high = len(arr) - 1
+    while low <= high:
+        middle = (low + high) // 2
+
+        if arr[middle] == target: 
+            return middle
+        
+        elif target < arr[middle]: 
+            high = middle - 1
+        
+        else: 
+            low = middle + 1
+    return -1
+
+def interpolation_search(arr: list[int] | tuple[int], target: int):
+    """
+        Finds a target element's index. Array must be sorted for the search to work. O(log n) time complexity. 
+    """
+    low = 0 
+    high = len(arr) - 1
+    while low <= high:
+        pos = low + (((target - arr[low]) * (high - low)) // (arr[high] - arr[low]))
+
+        if arr[pos] == target: 
+            return pos
+        
+        elif target < arr[pos]: 
+            high = pos - 1
+        
+        else: 
+            low = pos + 1
+    return -1
